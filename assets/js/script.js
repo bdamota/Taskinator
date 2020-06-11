ab0fcf365df91e7ca4179da375f1a40bd317577d
LYNN DAMOTA <bdamota0506@gmail.com>
2020-06-10 17:38:03 -0700

    added functionality to delete button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
+var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter = 0;
-
 var formEl = document.querySelector("#task-form");
 
 var buttonEl = document.querySelector("#save-task");
@@ -92,3 +92,19 @@ var createTaskActions = function(taskId) {
 
   formEl.addEventListener("submit", taskFormHandler);
 
+  var taskButtonHandler = function(event) {
+    console.log(event.target);
+  
+    if (event.target.matches(".delete-btn")) {
+      // get the element's task id
+      var taskId = event.target.getAttribute("data-task-id");
+      deleteTask(taskId);
+    }
+  };
+
+  var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+  };
+
+  pageContentEl.addEventListener("click", taskButtonHandler);
