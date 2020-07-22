38041bab11702ed5b6f9e0db2bb70811f0acc545
LYNN DAMOTA <bdamota0506@gmail.com>
2020-07-21 20:12:13 -0700

    task persists

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,6 @@ var tasks = [
     status: ""
   },
 ];
-
 //if data display something else or nothing load data function 
 
 var taskFormHandler = function(event) {
@@ -285,18 +284,43 @@ var saveTasks = function() {
 var loadTasks = function() {
   var savedTasks = localStorage.getItem("tasks");
 
-  if (!savedTasks) {
+  if (savedTasks === null) {
     return false;
   }
 
   savedTasks = JSON.parse(savedTasks);
 
-  // loop through savedTasks array
-for (var i = 0; i < savedTasks.length; i++) {
-  // pass each task object into the `createTaskEl()` function
-  createTaskEl(savedTasks[i]);
-}  
-};
+  for (var i = 0; i < savedTasks.length; i++) {
+    // pass each task object into the `createTaskEl()` function
+    createTaskEl(savedTasks[taskIdCounter]);
+
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.setAttribute("data-task-id", tasks[i].id);
+    listItemEl.setAttribute("draggable", "true");
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    var taskActionsEl = createTaskActions(tasks[i].id);
+    listItemEl.appendChild(taskActionsEl);
+  
+    if (tasks[i].status === "to do") {
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+      tasksToDoEl.appendChild(listItemEl);
+     }
+     else if (tasks[i].status === "in progress") {
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+      tasksInProgressEl.appendChild(listItemEl)
+     }
+     else if (tasks[i].status === "completed") {
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+      tasksCompletedEl.appendChild(listItemEl)
+     }
+    }  
+  };
 
 loadTasks();
 
@@ -306,7 +330,4 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
 pageContentEl.addEventListener("dragleave", dragLeaveHandler);
-
-
-
 
